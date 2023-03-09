4ab806f131e15c352daf239cc0ada3bfb07810a6
JR McGregor <43829830+MerfMan47@users.noreply.github.com>
2023-03-08 17:29:56 -0800

    Add files via upload

---
+++ Attempts/Medium/Longest Palindromic Substring/Day 4/main.js
@@ -0,0 +1,99 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+let ansStr = ''
+let arr
+const letterSet = new Set()
+let ansArr = []
+
+function reverse(str){
+    return str.split('').reverse().join('')
+}
+
+// function verification(subStr){
+//     if(subStr == reverse(subStr)){
+//         if(subStr.length > ansStr){
+//             ansStr = subStr
+//         }
+//     }
+// }
+
+function verify(array){
+    return array.reduce((longestPalStr, subStr) => {
+        if((subStr.length > longestPalStr)&&(subStr == reverse(subStr))){
+            return subStr
+        }else{
+            return longestPalStr
+        }
+    }, '')
+}
+
+function longest(array){
+    return array.reduce((longestStr, str) => {
+        if(str.length > longestStr){
+            return str
+        }else{
+            return longestStr
+        }
+    }, '')
+}
+
+function indexLetters(str){
+    for (let char of str){
+        letterSet.add(char)
+    }
+}
+
+function replaceEmpty(array, char){
+    //loop through array, count empty, replace all empty in sepction with current char. add char to start and end of each item sans first and last ANSWER WILL NEVER BE FIRST OR LAST ARRAY ENTRY MAYBE CAN REMOVE THEM
+    
+
+    
+
+}
+
+
+
+var longestPalindrome = function(s) {
+    indexLetters(s)
+    
+    console.log(s)
+    console.log(letterSet)
+    for(const item of letterSet){
+        arr = s.split(item)
+
+        //test area
+        for(let i = s.lenght; i>0; i--){
+            if(item == s.charAt(i)){
+                arr.push(s.substring())
+            }
+        } 
+
+
+
+
+
+        //test area
+
+        // arr = s.substring()
+
+        console.log('split at: ' + item)
+        console.log('split arr' + arr)
+        console.log('palindromic entires' + verify(arr))
+
+        //for empties
+        console.log(arr.length - arr.filter(String).length+1)
+
+        // arr = replaceEmpty(arr, item)
+
+
+        ansArr.push(verify(arr))
+
+        console.log(ansArr)
+        console.log(longest(ansArr))
+
+        return longest(ansArr)
+    }
+};
---
+++ Attempts/Medium/Longest Palindromic Substring/Day 4/readme.txt
@@ -0,0 +1,6 @@
+Back at it again, I've realized that using split
+will not work as the palandromic substrings may 
+have the current item inside the palandrome.
+Thus I need to approach from the end of the string
+and find the first substring that is a plandrome
+then move onto the next item.
